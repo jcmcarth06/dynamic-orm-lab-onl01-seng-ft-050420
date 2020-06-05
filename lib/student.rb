0a727ead0a7728f7ae8343fa89bf2bd9557a1fa9
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-05 18:27:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,4 +21,10 @@ class Student < InteractiveRecord
     column_names.compact
   end
 
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
 end
