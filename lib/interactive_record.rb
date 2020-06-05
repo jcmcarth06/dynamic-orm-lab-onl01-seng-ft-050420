68e00529d298f6e2163e515e457052e41a722519
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-05 18:30:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,6 +3,10 @@ require 'active_support/inflector'
 
 class InteractiveRecord
 
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
