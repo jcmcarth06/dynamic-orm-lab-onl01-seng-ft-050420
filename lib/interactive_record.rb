4c9ab89ad103d79ad80e00fbe25b6739bd2dc352
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-05 18:40:16 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,6 +3,12 @@ require 'active_support/inflector'
 
 class InteractiveRecord
 
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+  
   def self.table_name
     self.to_s.downcase.pluralize
   end
