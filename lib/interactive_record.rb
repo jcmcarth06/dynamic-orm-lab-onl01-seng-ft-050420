fd709472d40812389b5fdacfd8a0887397bb31aa
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-05 18:37:33 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,5 +20,17 @@ class InteractiveRecord
     column_names.compact
   end
 
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
 
 end
