8190bdcc06495f79b037b7b682588987490b7ceb
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-05 18:29:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,10 +21,8 @@ class Student < InteractiveRecord
     column_names.compact
   end
 
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
   end
 
 end
